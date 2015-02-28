b05306f2cecb3286c71ff54e9411b2ded1b5aee6
Smal Sergey <smal.sergey@gmail.com>
2015-02-28 22:04:55 +0300

    Added custom login page

--- src/main/java/com/smalser/pdat/SimpleController.java
+++ src/main/java/com/smalser/pdat/SimpleController.java
@@ -3,6 +3,7 @@ package com.smalser.pdat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -25,6 +26,25 @@ public class SimpleController
         model.addObject("title", "Spring Security title");
         model.addObject("body", "This is protected page!");
         model.setViewName("admin");
+        return model;
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView login(@RequestParam(value = "error", required = false) String error,
+                              @RequestParam(value = "logout", required = false) String logout)
+    {
+        ModelAndView model = new ModelAndView();
+        if (error != null)
+        {
+            model.addObject("error", "Wrong username or password");
+        }
+
+        if (logout != null)
+        {
+            model.addObject("msg", "You have been logged out successfully!");
+        }
+
+        model.setViewName("login");
         return model;
     }
 }
--- src/main/webapp/WEB-INF/pages/index.jsp
+++ src/main/webapp/WEB-INF/pages/index.jsp
@@ -1,7 +1,11 @@
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@page session="false"%>
 <html>
 <body>
 <h1>Title : ${title}</h1>
 <h1>Message : ${body}</h1>
+<c:if test="${pageContext.request.userPrincipal.name != null}">
+    Hello, ${pageContext.request.userPrincipal.name} !
+</c:if>
 </body>
 </html>
